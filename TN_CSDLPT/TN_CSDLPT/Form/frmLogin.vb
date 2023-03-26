eb58b6e5b58a675cfeaa04ecc4079599a4c8b52b
ahuynh359 <ahuynh359@gmail.com>
2023-03-27 03:28:57 +0700

    Init Project

---
+++ TN_CSDLPT/TN_CSDLPT/Form/frmLogin.vb
@@ -0,0 +1,9 @@
+﻿Public Class frmLogin
+    Private Sub CheckButton1_CheckedChanged(sender As Object, e As EventArgs)
+
+    End Sub
+
+    Private Sub CheckEdit1_CheckedChanged(sender As Object, e As EventArgs) Handles ceRememberMe.CheckedChanged
+
+    End Sub
+End Class
